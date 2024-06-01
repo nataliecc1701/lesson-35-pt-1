2fa2b4f549de3d40d654e6cc0f1e412206c705c6
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-05-31 22:04:39 -0700

    company route now returns invoices

--- routes/companies.js
+++ routes/companies.js
@@ -16,10 +16,13 @@ router.get("/", async (req, res, next) => {
 
 router.get("/:code", async (req, res, next) => {
     try {
-        const results = await db.query(`SELECT * FROM companies WHERE code=$1`,
+        const company = await db.query(`SELECT * FROM companies WHERE code=$1`,
             [req.params.code]);
-        if (!results.rows.length) throw new ExpressError("Not Found!", 404);
-        return res.json({company : results.rows[0]});
+        if (!company.rows.length) throw new ExpressError("Not Found!", 404);
+        const invoices = await db.query(`SELECT * FROM invoices WHERE comp_code = $1`,
+            [req.params.code]);
+        company.rows[0].invoices = invoices.rows;
+        return res.json({company : company.rows[0]});
     }
     catch (e) {
         return next(e)
