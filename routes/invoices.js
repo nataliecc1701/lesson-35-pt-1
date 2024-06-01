458cb4cafbd4d0c551f4bba41b13ccb00f4bf01b
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-05-31 21:35:30 -0700

    added GET routes

--- routes/invoices.js
+++ routes/invoices.js
@@ -4,5 +4,26 @@ const db = require("../db")
 
 router = new express.Router()
 
+router.get("/", async (req, res, next) => {
+    try {
+        const results = await db.query(`SELECT * FROM invoices`)
+        return res.json({invoices : results.rows})
+    }
+    catch (e) {
+        return next(e)
+    }
+})
+
+router.get("/:id", async (req, res, next) => {
+    try {
+        const results = await db.query(`SELECT * FROM id WHERE id=$1`,
+            [req.params.id]);
+        if (!results.rows.length) throw new ExpressError("Not Found!", 404);
+        return res.json({invoice : results.rows[0]});
+    }
+    catch (e) {
+        return next(e)
+    }
+})
 
 module.exports = router
