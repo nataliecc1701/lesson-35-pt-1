f9dbba4ca4b2e5d52de9661f65dcbf8a2d84629d
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-06-02 23:13:02 -0700

    got a test working

--- app.test.js
+++ app.test.js
@@ -4,12 +4,22 @@ const request = require("supertest");
 const app = require("./app")
 const db = require("./db")
 
+let testCo;
+let testInv;
+
 beforeEach(async () => {
-    company = db.query(`INSERT INTO companies (code, name, description) VALUES ($1 $2 $3)`,
-    ['testco', 'Test Company', 'A fake company inserted into the database during automated tests'])
-    invoice = db.query(`INSERT INTO invoices (comp_code, amt) VALUES ($1, $2)`
-    ['testco', 314])
-    await Promise.all([company, invoice])
+    company = await db.query(
+        `INSERT INTO companies (code, name, description)
+        VALUES ('testco', 'Test Company', 'A fake company inserted into the database during automated tests')
+        RETURNING code, name, description`
+    );
+    invoice = await db.query(
+        `INSERT INTO invoices (comp_code, amt)
+        VALUES ('testco', 314)
+        RETURNING comp_code, amt`
+    );
+    testCo = company.rows[0];
+    testInv = invoice.rows[0];
 })
 
 afterEach(async () => {
@@ -17,4 +27,16 @@ afterEach(async () => {
     q1 = db.query(`DELETE FROM companies`)
     q2 = db.query(`DELETE FROM invoices`)
     await Promise.all([q1, q2])
+})
+
+afterAll(async () => {
+    await db.end();
+})
+
+describe("GET /companies", () => {
+    test("gets a list of companies", async () => {
+        const resp = await request(app).get("/companies");
+        expect(resp.statusCode).toBe(200);
+        expect(resp.body).toEqual({companies: [testCo]})
+    })
 })
