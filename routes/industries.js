1926d7d2e3a16374eb6fde5ec9b2d437782439c7
Natalie Chapman <natalie.c.chapman628@gmail.com>
2024-06-03 02:11:44 -0700

    added route to get all industries

--- routes/industries.js
+++ routes/industries.js
@@ -4,7 +4,39 @@ const db = require("../db")
 
 router = new express.Router()
 
-
+router.get("/", async (req, res, next) => {
+    try {
+        const results = await db.query(
+            `SELECT * FROM industries
+            LEFT JOIN companies_industries ON ind_code = code`);
+        // return res.json(results.rows)
+        const industries = []
+        for (const r of results.rows) {
+            ind = { code: r.code, industry: r.industry, companies: []};
+            
+            // check to see if ind is in our industries array
+            let idx = -1;
+            for (let i=0; i<industries.length; i++) {
+                if (industries[i].code == ind.code) {
+                    idx = i
+                    break
+                }
+            }
+            // add it if it isn't
+            if (idx === -1) {
+                idx = industries.length;
+                industries.push(ind);
+            }
+            
+            // add the company to the industries array
+            industries[idx].companies.push(r.comp_code)
+        }
+        return res.json({industries})
+    }
+    catch (e) {
+        return next(e)
+    }
+})
 
 
 module.exports = router
